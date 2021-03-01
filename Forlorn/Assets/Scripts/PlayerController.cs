25ea1ebd8f0b6f1db5819abac40f667eccffc666
Bri <briannatan45@gmail.com>
2021-02-28 23:34:03 -0700

    Added fixed animations and menu system

--- Forlorn/Assets/Scripts/PlayerController.cs
+++ Forlorn/Assets/Scripts/PlayerController.cs
@@ -22,49 +22,53 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
         // Player moves toward the PlayerMovePoint
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
         // Checks the distance first before moving
-        if(Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
         {
             // Checking if there is any horizontal input, left or right
-            if( Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            if (Mathf.Abs(horizontalInput) == 1f)
             {
-
                 // Checks to see if there is something ahead
-                if ( !Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, stopMovement))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(horizontalInput, 0f, 0f), 0.2f, stopMovement))
                 {
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                    movePoint.position += new Vector3(horizontalInput, 0f, 0f);
                 }
             }
             // Checking if there is any vertical input, up or down
-            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            else if (Mathf.Abs(verticalInput) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, stopMovement))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, verticalInput, 0f), 0.2f, stopMovement))
                 {
-                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    movePoint.position += new Vector3(0f, verticalInput, 0f);
                 }
             }
         }
 
+
+        bool isFront = verticalInput < 0;
+        bool isBack = verticalInput > 0;
+        bool isLeft = horizontalInput < 0;
+        bool isRight = horizontalInput > 0;
+        bool isIdle = horizontalInput == 0 && verticalInput == 0;
+
         // Animator
-        if (Input.GetKey("down"))
-        {
-            playerAnimator.SetBool("isFront", true);
-        }
-        else if (Input.GetKey("up"))
-        {
-            playerAnimator.SetBool("isBack", true);
-        }
-        else if (Input.GetKey("left"))
-        {
-            playerAnimator.SetBool("isLeft", true);
-        }
-        else if (Input.GetKey("right"))
-        {
-            playerAnimator.SetBool("isRight", true);
-        }
-      
+        playerAnimator.SetBool("isFront", isFront);
+
+        playerAnimator.SetBool("isBack", isBack);
+
+        playerAnimator.SetBool("isLeft", isLeft);
+
+        playerAnimator.SetBool("isRight", isRight);
+
+        playerAnimator.SetBool("isIdle", isIdle);
+
+
+
     }
 }
