8052748ba6237db231d461e444defdb04b691880
Bri <briannatan45@gmail.com>
2021-03-01 07:37:39 -0700

    Changed Audio Manager

--- Forlorn/Assets/Scripts/AudioManager.cs
+++ Forlorn/Assets/Scripts/AudioManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public AudioSource BackgroundMusic;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
